81289649803cd6b4f5b19b2d74a0179e9d53fd02
NhatQuangIT1301 <quangit1301@gmail.com>
2026-09-09 20:40:41 +0700

    chore: initial Spring Boot project structure

---
+++ src/main/java/com/nhatquang/high_concurrency_ticketing_engine/HighConcurrencyTicketingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.nhatquang.high_concurrency_ticketing_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyTicketingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyTicketingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhatquang/high_concurrency_ticketing_engine/HighConcurrencyTicketingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhatquang.high_concurrency_ticketing_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyTicketingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
